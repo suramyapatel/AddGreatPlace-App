78ddbe834c40f6708490a34dc9d664113ae76b9d
Suramya Patel <suramyapatel@gmail.com>
2021-11-19 13:42:32 -0500

    First commit

---
+++ components/ImagePicker.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { View, Button, Text, StyleSheet, Image, Alert } from "react-native";
+import Colors from "../constants/Colors";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+const ImgPicker = (props) => {
+  const [pickedImage, setPickedImage] = useState();
+
+  const veryfyPermission = async () => {
+    const result = await Permissions.askAsync(Permissions.CAMERA);
+    if (result.status !== "granted") {
+      Alert.alert(
+        "InSufficient permissions!",
+        "You need to grant permissions to use this app",
+        [{ text: "Okay" }]
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const takeImageHandler = async () => {
+    const hasPermissions = await veryfyPermission();
+    if (!hasPermissions) {
+      return;
+    }
+    const image = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setPickedImage(image.uri);
+    props.onImageTaken(image.uri);
+  };
+  return (
+    <View style={styles.imagePicker}>
+      <View style={styles.imagePreview}>
+        {!pickedImage ? (
+          <Text>No image picked yet.</Text>
+        ) : (
+          <Image style={styles.image} source={{ uri: pickedImage }} />
+        )}
+      </View>
+      <Button
+        title="Take Image"
+        color={Colors.primary}
+        onPress={takeImageHandler}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imagePicker: {
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#ccc",
+    borderWidth: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ImgPicker;
---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import { useDispatch } from "react-redux";
+import LocationPicker from "../components/LocationPicker";
+
+import Colors from "../constants/Colors";
+import * as PlacesActions from "../store/places-actions";
+import ImgPicker from "../components/ImagePicker";
+const NewPlaceScreen = (props) => {
+  const [titleValue, setTitleValue] = useState("");
+  const [selectedImage, setSelectedImage] = useState();
+
+  const dispatch = useDispatch();
+
+  const titleChangeHandler = (text) => {
+    setTitleValue(text);
+  };
+
+  const imageTakenHandler = (imagePath) => {
+    setSelectedImage(imagePath);
+  };
+
+  const savePlaceHolder = () => {
+    dispatch(PlacesActions.addPlace(titleValue, selectedImage));
+    props.navigation.goBack();
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={titleValue}
+        />
+        <ImgPicker onImageTaken={imageTakenHandler} />
+        <LocationPicker />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHolder}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+NewPlaceScreen.navigationOptions = {
+  headerTitle: "Add Place",
+};
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
+
+export default NewPlaceScreen;
